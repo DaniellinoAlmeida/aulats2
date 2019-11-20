c57adec311cccd12e2f0e48cf0465d068415a299
daniellinoalmeida <ggg4m3r@gmail.com>
2019-11-20 00:26:29 +0000

    banco

---
+++ banco.ts
@@ -0,0 +1,32 @@
+namespace empresa{
+    export class Banco{
+        private _nome:string;
+        private _codBanco:number;
+        private _clientes:Array<Cliente> = [];
+        private _funcionarios:Array<Funcionario> = [];
+
+        public getNome(){
+            return this._nome;
+        }
+
+        public setNome(nome:string){
+            this._nome = nome;
+        }
+
+        public getCodBanco(){
+            return this._codBanco;
+        }
+
+        public setCodBanco(codigo:number){
+            this._codBanco = codigo;
+        }
+
+        public getFuncionario(){
+            return this._funcionarios;
+        }
+
+        public addFuncionario(funcionario:Funcionario){
+            this._funcionarios.push(funcionario);
+        }
+    }
+}
