8bbcdfee43dc2ce760a7f788f20efd731329bdee
daniellinoalmeida <ggg4m3r@gmail.com>
2019-11-20 01:29:34 +0000

    add banco no cliente

--- cliente.ts
+++ cliente.ts
@@ -4,6 +4,8 @@ namespace empresa{
     export class Cliente extends Pessoa{
         private _codCliente:Number;
         private _credito:Number;
+        private _banco:Banco;
+
 
         public getCodCliente(){
             return this._codCliente;
@@ -19,6 +21,14 @@ namespace empresa{
 
         public setCredito(credito:Number){
             this._credito = credito;
+        }
+
+        public getBanco(){
+            return this._banco;
+        }
+
+        public setBanco(banco:Banco){
+            this._banco = banco;
         }
     }
 }
